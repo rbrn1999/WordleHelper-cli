6ed3d3e59b84b319efbd2166aa0c11b383f89e08
Ian Liu <ianliu@livemail.tw>
2022-01-28 02:51:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# WordleHelper
+
+## Description
+A cli tool to assist your daily wordle game if you need help
+
+## How to Run
+`python main.py`
+
+## TODO
+handle duplicate letters attemp or answer cases
+remove yellow letter when the correct position is known
+highlight corresonding letters in canidate words
---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+
+#get word list
+
+#method using nltk
+# import nltk
+# # nltk.download() # use when first run
+# word_list = nltk.corpus.words.words()
+
+#method using system words, only for Linux or macOS
+
+words = open("/usr/share/dict/words", "r")
+word_list = words.read().split()
+words.close()
+print(len(word_list))
+
+#get 5 character long words
+word_list = [word for word in word_list if len(word) == 5]
+
+green_letters = {}
+yellow_letters = {}
+gray_letters = set()
+
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    mode = int(input(
+        '''
+1. add a green letter
+2. add a yellow letter
+3. add gray letters
+4. show {} candidates
+5. reset
+enter any other number to exit: '''
+        .format(len(word_list))))
+
+    if mode == 1:
+        positionIndex = int(input("position: ")) - 1
+        letter = input("letter: ")
+        green_letters[positionIndex] = letter
+    elif mode == 2:
+        positionIndex = int(input("position: ")) - 1
+        letter = input("letter: ")
+        yellow_letters[positionIndex] = letter
+    elif mode == 3:
+        letters = input("letters(seperate by space): ").split()
+        for letter in letters:
+            gray_letters.add(letter)
+    elif mode == 4:
+        for word in word_list:
+            print(word, end=' ')
+        input("\npress enter to continue ...")
+        continue
+    elif mode == 5:
+        words = open("/usr/share/dict/words", "r")
+        word_list = words.read().split()
+        words.close()
+        green_letters = {}
+        yellow_letters = {}
+        gray_letters = set()
+        continue
+    else:
+        confirm = input("are you sure you want to exit? y/[n]")
+        if confirm == 'y':
+            break
+        else:
+            continue
+
+    #gray
+    tempList = []
+    for word in word_list:
+        valid = True
+        for c in gray_letters:
+            if c in word:
+                valid = False
+                break
+        if valid:
+            tempList.append(word)
+
+    word_list = tempList.copy()  
+
+    #green
+    tempList = []
+    for word in word_list:
+        # check each green letter
+        valid = True
+        for i in range(5):
+            if i not in green_letters:
+                continue
+            if word[i].lower() != green_letters[i].lower():
+                valid = False
+                break
+        if valid:
+            tempList.append(word)
+    word_list = tempList.copy()
+
+    #yellow
+    tempList = []
+    for word in word_list:
+        # check each yellow letter
+        valid = True
+        for i in range(5):
+            if i not in yellow_letters:
+                continue
+            if yellow_letters[i].lower() not in word or yellow_letters[i].lower() == word[i].lower():
+                valid = False
+                break
+        if valid:
+            tempList.append(word)
+    word_list = tempList.copy()
+
